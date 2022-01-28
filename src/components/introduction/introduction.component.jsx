7e96e179f729a55ddf05f59ef5e15ef388254bbc
unknown <romano2737@gmail.com>
2022-01-28 09:28:41 +0300

    added template introduction

---
+++ src/components/introduction/introduction.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './introduction.style.scss'
+
+const Introduction = ({imagesrc, text}) => {
+  return <div className='item'>{text}</div>;
+};
+
+export default Introduction;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import './homepage.style.scss';
 
+import Introduction from "../../components/introduction/introduction.component";
+
 const HomePage = () => (
     
   <div className="homepage">
     <h1>Welcome to Task Manager</h1>
+
+    <div className="introduction-cotainer">
+    <Introduction imagesrc='' text='Create tasks for your company'/>
+    <Introduction imagesrc='' text='Assign users'/>
+    <Introduction imagesrc='' text='Manage your tasks'/>
+    <Introduction imagesrc='' text='Add new users'/>
+    <Introduction imagesrc='' text='Stay up-to-date'/>
+    <Introduction imagesrc='' text='Assign tasks any time anywhere'/>    
+    <Introduction imagesrc='' text='Report to your customers'/>    
+    <Introduction imagesrc='' text='Improve your workflow'/>
+    
+    </div>
   </div>
 )
 
